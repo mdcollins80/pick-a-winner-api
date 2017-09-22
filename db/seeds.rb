1a5a9d34836fe89044d361ecece0f51664c4ff8b
mdcollins80 <mdcollins80@gmail.com>
2017-09-22 15:59:54 -0400

    Seed Sample Teams
    Add seed data to db/seeds.rb for eight sample teams.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,55 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Team.create!(
+  [
+    {
+      city: 'Buffalo',
+      name: 'Bills',
+      conference: 'AFC',
+      division: 'East'
+    },
+    {
+      city: 'Miami',
+      name: 'Dolphins',
+      conference: 'AFC',
+      division: 'East'
+    },
+    {
+      city: 'New England',
+      name: 'Patriots',
+      conference: 'AFC',
+      division: 'East'
+    },
+    {
+      city: 'New York',
+      name: 'Jets',
+      conference: 'AFC',
+      division: 'East'
+    },
+    {
+      city: 'Baltimore',
+      name: 'Ravens',
+      conference: 'AFC',
+      division: 'North'
+    },
+    {
+      city: 'Cincinnati',
+      name: 'Bengals',
+      conference: 'AFC',
+      division: 'North'
+    },
+    {
+      city: 'Cleveland',
+      name: 'Browns',
+      conference: 'AFC',
+      division: 'North'
+    },
+    {
+      city: 'Pittsburgh',
+      name: 'Steelers',
+      conference: 'AFC',
+      division: 'North'
+    }
+  ]
+)
