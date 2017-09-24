2efdc788c26b0ec82e6c81a10fd06bf4aeb42c8b
mdcollins80 <mdcollins80@gmail.com>
2017-09-24 08:38:58 -0400

    Add Picks table, Add relationships to Games, Teams
    -Run migration to create Picks table
    -Add has_many to Teams for away_team, home_team, and winning_team
    -Add belongs_to to Games for away_team, home_team, and winning_team

--- db/migrate/20170922210850_create_picks.rb
+++ db/migrate/20170922210850_create_picks.rb
@@ -1,9 +1,9 @@
 class CreatePicks < ActiveRecord::Migration[5.0]
   def change
     create_table :picks do |t|
-      t.integer :user_id
-      t.integer :game_id
-      t.integer :picked_team_id
+      t.references :user, index: true, foreign_key: true, null: false
+      t.references :game, index: true, foreign_key: true, null: false
+      t.references :team, index: true, foreign_key: true, null: false
 
       t.timestamps
     end
