27d01492a7c07761b5463561b53bf570af48643f
mdcollins80 <mdcollins80@gmail.com>
2017-10-04 18:34:47 -0400

    Update curl scripts to hit Heroku
    -Add API url pointing at Heroku to sign-in, get-games, and update-game.

--- scripts/games/get-games.sh
+++ scripts/games/get-games.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-API="${API_ORIGIN:-http://localhost:4741}"
+API="${API_ORIGIN:-https://pick-a-winner-nfl.herokuapp.com}"
+# API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/games"
 curl "${API}${URL_PATH}" \
   --include \
--- scripts/games/update-game.sh
+++ scripts/games/update-game.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-
-API="${API_ORIGIN:-http://localhost:4741}"
+API="${API_ORIGIN:-https://pick-a-winner-nfl.herokuapp.com}"
+# API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/games/${ID}"
 curl "${API}${URL_PATH}" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
   --data '{
     "game": {
-      "week_num": "'"${WEEKNUM}"'"
+      "away_team": "'"${AWAYTEAM}"'"
     }
   }'
 
