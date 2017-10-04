21b3ecfad4271e3d717620a8770d33c5e29e0659
mdcollins80 <mdcollins80@gmail.com>
2017-10-04 18:31:43 -0400

    Migrations for user admin properties
    -Create migration for admin property to user table.
    -Create migration to add uniqueness index to user's team_name.
    -Run migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926202607) do
+ActiveRecord::Schema.define(version: 20171004204811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,13 +45,15 @@ ActiveRecord::Schema.define(version: 20170926202607) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",           null: false
-    t.string   "token",           null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "email",                           null: false
+    t.string   "token",                           null: false
+    t.string   "password_digest",                 null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "team_name"
+    t.boolean  "admin",           default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["team_name"], name: "index_users_on_team_name", unique: true, using: :btree
     t.index ["token"], name: "index_users_on_token", unique: true, using: :btree
   end
 
